528f8332e115e9b894cadb348660062c2ef9760f
MOhdalialavi <120246642+MOhdalialavi@users.noreply.github.com>
2026-01-16 22:27:19 +0530

    changed some html and url codes

--- tourism/urls.py
+++ tourism/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search_place, name='search_place'),
     path('place/<slug:slug>/', views.place_detail, name='place_detail'),
+    path('suggest/', views.suggest_places, name='suggest_places'),
 ]
 
